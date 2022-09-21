05598fbaac4293f6de063e1f6b78378e376999a1
Alex_Wachira_Kimotho <alex.wachira@student.moringashool.com>
2022-09-21 14:47:59 +0300

    Assignment completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from " react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
 
 ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
